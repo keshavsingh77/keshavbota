4ad3bfed54b54d2cd1ed9a312b577b764b776746
keshavsingh77 <keshavrajlkr2@gmail.com>
2025-06-03 03:07:37 -0700

    Delete shareUsWalaBot1-main directory

--- shareUsWalaBot1-main/start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/RoyalKrrishna/shareUsWalaBot1.git /shareUsWalaBot1
-cd /shareUsWalaBot1
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 main.py
