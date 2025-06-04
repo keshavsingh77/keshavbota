c92a77b703133620ac02d7cd12da82feff02002c
keshavsingh77 <keshavrajlkr2@gmail.com>
2025-06-04 07:42:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/keshavsingh77/keshavbotA.git /keshavbotA
-cd /shareUsWalaBot1
+cd /keshavbotA
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
